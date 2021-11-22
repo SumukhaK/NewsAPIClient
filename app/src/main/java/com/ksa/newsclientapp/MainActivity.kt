c4c83b490ad3ab2698d6523c57595517354b4ec6
sumukha <sumukh.aithal@gmail.com>
2021-11-22 16:30:37 +0530

    added headlines UI page

--- app/src/main/java/com/ksa/newsclientapp/MainActivity.kt
+++ app/src/main/java/com/ksa/newsclientapp/MainActivity.kt
@@ -2,16 +2,22 @@ package com.ksa.newsclientapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.ksa.newsclientapp.databinding.ActivityMainBinding
+import com.ksa.newsclientapp.presentation.viewmodel.NewsViewModel
+import com.ksa.newsclientapp.presentation.viewmodel.NewsViewModelFactory
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var factory: NewsViewModelFactory
     private lateinit var binding:ActivityMainBinding
-
+    lateinit var viewModel: NewsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         binding.bottomNavigation.setupWithNavController(navController)
 
-        
+        viewModel = ViewModelProvider(this,factory).get(NewsViewModel::class.java)
+
     }
 }
